0226f61129bea9e269b9cf6e6ddbd5bb6c35850e
Umairahmed37 <emailumair125125@gmail.com>
2021-12-17 11:40:29 +0500

    db connected

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-
 const express = require('express')
+const connection=require('./connect')
 const app = express()
-const port = 3000
+// const cors=require('cors')
  
-app.get('/', (req, res) => res.send('Hello my World!'))
-app.listen(port, () => console.log(`Example  app listening on port ${port}!`))
+connection();
+ 
+ 
+app.get('/', (req, res) => res.send('Hello World!'))
+
+
+const port = process.env.PORT || 5000
+app.listen( port , () => console.log(`Example app listening on port ${port}!`))
